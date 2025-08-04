2cce156f0ae07e2f4949e1bd5a80fab8b759e0b9
GreenCheeseMan7777 <colelingle7@gmail.com>
2025-08-04 12:44:32 -0400

    B: Chapters 1 and 2 completed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from constants import *
+from player import *
+from circleshape import *
 import pygame
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -8,12 +10,23 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while 0 == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+        screen.fill("black")
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
